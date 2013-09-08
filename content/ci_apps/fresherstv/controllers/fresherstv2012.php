b1a9a2be4685307a1a0b6946a9a8c02531808b90
Tom Jenkinson <tjcode@outlook.com>
2013-09-08 16:16:03 +0100

    fix

---
+++ content/ci_apps/fresherstv/controllers/fresherstv2012.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Fresherstv2012 extends CI_Controller {
+
+	public function index()
+	{
+		output_page("fresherstv2012", array(), array(), $this->load->view('page/freshers_tv_2012', array(), TRUE));
+	}
+}
