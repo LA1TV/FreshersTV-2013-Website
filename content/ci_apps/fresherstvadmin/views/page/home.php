484bda70f36cffbe5b8d3021cadcc0398017b237
root <root@echo356.server4you.net>
2013-09-10 02:33:56 +0200

    fixed admin panel home page link

--- content/ci_apps/fresherstvadmin/views/page/home.php
+++ content/ci_apps/fresherstvadmin/views/page/home.php
@@ -1,6 +1,5 @@
 <h1>Home</h1>
 <p>Welcome to the control panel!</p>
 <div class="btn-group-vertical btn-group-sm">
-	<a class="btn btn-default" href="<?=base_url();?>applications">View Applications</a>
-	<a class="btn btn-default" href="<?=base_url();?>vts">View VT Submissions</a>
+	<a class="btn btn-default" href="<?=base_url();?>viewapplications">View Applications</a>
 </div>
