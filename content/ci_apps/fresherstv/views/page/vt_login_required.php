5bf6ef53abc07edc637e6db3cf43967900bf06b0
root <root@echo356.server4you.net>
2013-09-14 11:28:28 +0100

    update

--- content/ci_apps/fresherstv/views/page/vt_login_required.php
+++ content/ci_apps/fresherstv/views/page/vt_login_required.php
@@ -1,5 +1,5 @@
 <h2>Submit VT</h2>
 <p><strong>You need to be logged in in order to submit your VT.</strong><p>
-<p><a class="btn btn-primary btn-sm show-login-dialog" href="<?=base_url();?>login">Click Here To Log In</a></p>
+<p><a class="btn btn-primary btn-sm show-login-dialog" href="<?=base_url();?>login?next_uri=submitvt">Click Here To Log In</a></p>
 <p>If you don't have an account yet you need to create one by submitting the <a href="<?=base_url();?>apply">application form</a>.</p>
 <p>If you have any problems please send an e-mail to "<a href="mailto:development@la1tv.co.uk">development@la1tv.co.uk</a>".</p>
