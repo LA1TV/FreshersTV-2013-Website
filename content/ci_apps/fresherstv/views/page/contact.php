b10ef7d3c72fd5ad2e05fc7dffe65c536b4f38c1
root <root@echo356.server4you.net>
2013-09-14 12:21:13 +0100

    updated contact page

--- content/ci_apps/fresherstv/views/page/contact.php
+++ content/ci_apps/fresherstv/views/page/contact.php
@@ -1,9 +1,9 @@
 <h2>Contact Us</h2>
 <dl class="dl-horizontal contact-dl">
-	<dt>Chris Osborn (Live Events Coordinator)</dt>
+	<dt>Chris Osborn (Production Manager)</dt>
 	<dd><a href="mailto:c.osborn@la1tv.co.uk">c.osborn@la1tv.co.uk</a></dd>
-	<dt>Rachel Hughes (Secretary)</dt> 
+	<dt>Rachel Hughes (Communications Manager)</dt> 
 	<dd><a href="mailto:r.hughes@la1tv.co.uk">r.hughes@la1tv.co.uk</a></dd>
-	<dt>Technical</dt>
+	<dt>Technical Support</dt>
 	<dd><a href="mailto:development@la1tv.co.uk">development@la1tv.co.uk</a></dd>
 </dl>
